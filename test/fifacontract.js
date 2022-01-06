5a6ca9bceeb9766c0a888aebefe2b1a53ec6cef7
ulises <ulises.torrella@gmail.com>
2022-01-05 22:48:58 -0300

    Metacoin to FIFA

--- test/fifacontract.js
+++ test/fifacontract.js
@@ -1,38 +1,30 @@
-const MetaCoin = artifacts.require("FIFA");
+const FIFA = artifacts.require("FIFA");
 
 contract('FIFA', (accounts) => {
-  it('should put 10000 MetaCoin in the first account', async () => {
-    const metaCoinInstance = await MetaCoin.deployed();
-    const balance = await metaCoinInstance.balanceOf.call(accounts[0]);
+  it('should put 10000 FIFA in the first account', async () => {
+    const FIFAInstance = await FIFA.deployed();
+    const balance = await FIFAInstance.balanceOf.call(accounts[0]);
 
     assert.equal(balance.valueOf(), 10000, "10000 wasn't in the first account");
   });
-  // it('should call a function that depends on a linked library', async () => {
-    // const metaCoinInstance = await MetaCoin.deployed();
-    // const metaCoinBalance = (await metaCoinInstance.balanceOf.call(accounts[0])).toNumber();
-
-    // const metaCoinEthBalance = (await metaCoinInstance.getBalanceInEth.call(accounts[0])).toNumber();
-
-    // assert.equal(metaCoinEthBalance, 2 * metaCoinBalance, 'Library function returned unexpected function, linkage may be broken');
-  // });
   it('should send coin correctly', async () => {
-    const metaCoinInstance = await MetaCoin.deployed();
+    const FIFAInstance = await FIFA.deployed();
 
     // Setup 2 accounts.
     const accountOne = accounts[0];
     const accountTwo = accounts[1];
 
     // Get initial balances of first and second account.
-    const accountOneStartingBalance = (await metaCoinInstance.balanceOf.call(accountOne)).toNumber();
-    const accountTwoStartingBalance = (await metaCoinInstance.balanceOf.call(accountTwo)).toNumber();
+    const accountOneStartingBalance = (await FIFAInstance.balanceOf.call(accountOne)).toNumber();
+    const accountTwoStartingBalance = (await FIFAInstance.balanceOf.call(accountTwo)).toNumber();
 
     // Make transaction from first account to second.
     const amount = 10;
-    await metaCoinInstance.transfer(accountTwo, amount, { from: accountOne });
+    await FIFAInstance.transfer(accountTwo, amount, { from: accountOne });
 
     // Get balances of first and second account after the transactions.
-    const accountOneEndingBalance = (await metaCoinInstance.balanceOf.call(accountOne)).toNumber();
-    const accountTwoEndingBalance = (await metaCoinInstance.balanceOf.call(accountTwo)).toNumber();
+    const accountOneEndingBalance = (await FIFAInstance.balanceOf.call(accountOne)).toNumber();
+    const accountTwoEndingBalance = (await FIFAInstance.balanceOf.call(accountTwo)).toNumber();
 
 
     assert.equal(accountOneEndingBalance, accountOneStartingBalance - amount, "Amount wasn't correctly taken from the sender");
