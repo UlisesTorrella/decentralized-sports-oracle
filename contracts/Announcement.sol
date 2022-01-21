f55e6118079d20f8d30c425f68b5c73b06cb63c5
ulises <ulises.torrella@gmail.com>
2022-01-21 01:19:59 -0300

    delegate responsability to announcemente contract (does not compile yet)

--- contracts/Announcement.sol
+++ contracts/Announcement.sol
@@ -4,16 +4,93 @@ pragma solidity ^0.8.10;
 import "./Oracle.sol";
 
 // Somehow i need to check that the announcement was created by a proven staker
-interface Announcement {
-  enum AnnouncementState{NotEnoughVotes, Solidified, Disproven}
+abstract contract Announcement {
+  enum AnnouncementState{NotEnoughVotes, Solidified, Rejected}
 
-  mapping(address => bool) positiveVoters;
-  mapping(address => bool) negativeVoters;
-  uint32 positiveVotes;
-  uint32 negativeVotes;
-  AnnouncementState state;
+  mapping(address => bool) private _positiveVoters;
+  mapping(address => bool) private _negativeVoters;
+  uint32 private _positiveVotes;
+  uint32 private _negativeVotes;
+  AnnouncementState private _state;
 
-  function reduce(Game[] games, Teams[] teams) virtual external returns(games, teams);
+  Oracle private _oracle;
+  address public announcer;
+
+
+  event Solidified();
+  event Rejected();
+
+  constructor(address payable oracle) {
+    _oracle = Oracle(oracle);
+    announcer = tx.origin;
+  }
+
+  modifier yetToVote() {
+    require(
+        !_positiveVoters[msg.sender] &&
+        !_negativeVoters[msg.sender]
+        ,
+        "You've already voted"
+    );
+    _;
+  }
+
+  modifier isStaker() {
+    require(_oracle.isStaking(msg.sender), "You must be a staker to announce something here");
+    _;
+  }
+
+  modifier openBallot() {
+    require(_state == AnnouncementState.NotEnoughVotes, "This announcement has already been decided");
+    _;
+  }
+
+  function aprove() external openBallot isStaker yetToVote returns (uint32, uint32) {
+    _positiveVoters[msg.sender] = true;
+    _positiveVotes += 1;
+
+    uint256 totalStakers = _oracle.getTotalStakers();
+
+    if (_positiveVotes > totalStakers/2 && _state == AnnouncementState.NotEnoughVotes) { 
+        // solidify announcement
+        emit Solidified();
+        _state = AnnouncementState.Solidified;
+    }
+
+    return (_positiveVotes, _negativeVotes);
+  }
+
+  function disprove() external openBallot isStaker yetToVote returns(uint32, uint32) {
+    _negativeVoters[msg.sender] = true;
+    _negativeVotes += 1;
+
+    uint256 totalStakers = _oracle.getTotalStakers();
+
+    if (_negativeVotes > totalStakers/2 && _state == AnnouncementState.NotEnoughVotes) { 
+        // reject announcement
+        emit Rejected();
+        _state = AnnouncementState.Rejected;
+    }
+
+    return (_positiveVotes, _negativeVotes);
+  }
+
+  
+  function reduce(Oracle.Game[] calldata games, Oracle.Team[] calldata teams) virtual external returns(Oracle.Game[] calldata, Oracle.Team[] calldata);
+
+
+  // getters 
+  function getAnnouncementState() public view returns (AnnouncementState state) {
+      return _state;
+  }
+
+  function getAnnouncementPosVotes() public view returns (uint32 posVotes) {
+      return _positiveVotes;
+  }
+
+  function getAnnouncementNegVotes() public view returns (uint32 negVotes) {
+      return _negativeVotes;
+  }
 
 }
 
@@ -21,29 +98,28 @@ contract TeamAnnouncement is Announcement {
 
   string _name;
 
-  constructor(string name_) {
+  constructor(address payable oracle, string memory name_) Announcement(oracle) {
     _name = name_;
   }
 
-  function reduce(Game[] games, Teams[] teams) external returns(games, teams) {
-    teams.push(_name);
-    // return (games, teams);
+  function reduce(Oracle.Game[] calldata games, Oracle.Team[] calldata teams) override virtual external returns(Oracle.Game[] calldata, Oracle.Team[] calldata){
+    return (games, teams);
   }
 }
 
 contract GameAnnouncement is Announcement {
 
-  Game _game;
+  Oracle.Game _game;
 
-  constructor(address teamA, address teamB, uint32 date) {
-    _game = Game({date: date, a: teamA, b: teamB,
+  constructor(address payable oracle, address teamA, address teamB, uint32 date) Announcement(oracle) {
+    _game = Oracle.Game({date: date, a: teamA, b: teamB,
                   status: Constants.UNSTARTED,
                   result: Constants.NOTRESULTYET,
                   scoreA: Constants.NOTRESULTYET,
                   scoreB: Constants.NOTRESULTYET});
   }
 
-  function reduce(Game[] games, Teams[] teams) external returns(games, teams) {
+  function reduce(Oracle.Game[] calldata games, Oracle.Team[] calldata teams) override external returns(Oracle.Game[] calldata, Oracle.Team[] calldata){
     games.push(_game);
     // return (games, teams);
   }
@@ -52,21 +128,23 @@ contract GameAnnouncement is Announcement {
 contract GoalAnnouncement is Announcement {
 
   uint64 _gameIndex;
-  Goal _goal;
+  Oracle.Goal _goal;
 
-  constructor(uint64 gameIndex, uint256 minute, address teamAwarded, uint8 jersey) {
+  constructor(address payable oracle, uint64 gameIndex, uint256 minute, address teamAwarded, uint8 jersey) Announcement(oracle) {
     _gameIndex = gameIndex;
-    _goal = Goal({minute: minute, teamAwarded: teamAwarded, jersey: jersey});
+    _goal = Oracle.Goal({minute: minute, teamAwarded: teamAwarded, jersey: jersey});
   }
 
-  function reduce(Game[] games, Teams[] teams) external returns(games, teams) {
-    Game game = games[_gameIndex];
+  function reduce(Oracle.Game[] calldata games, Oracle.Team[] calldata teams) override external returns(Oracle.Game[] calldata, Oracle.Team[] calldata){
+    Oracle.Game calldata game = games[_gameIndex];
     if (game.a == _goal.teamAwarded) {
       game.goalsA.push(_goal);
     }
     require(game.b == _goal.teamAwarded || "Imposible to award a goal to this team in this game");
     game.goalsB.push(_goal);
     // is game a reference? do this just change the state? who knows
+    games[_gameIndex] = game;
+    return (games, teams);
   }
 
 }
--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -34,35 +34,16 @@ abstract contract Oracle {
         uint8 jersey;
     }
 
-    struct GameStatus {
-        address gameId;
-        uint8 status;
-    }
-
     Game[] internal _games;
     Game[] internal _cGames;
 
-    // mapping(uint256 => Goal) internal _goals; // announcementId -> Goal mapping
-    // mapping(uint256 => Goal) internal _cGoals; // announcementId -> Goal mapping
-
-    // mapping(uint256 => GameStatus) internal _cGameStatus; // announcementId -> status mapping
-
-    function getGame(address id) external view returns (Game memory) {
+    function getGame(uint256 id) external view returns (Game memory) {
         return _games[id];
     }
 
-    // Modifiers
-
-    modifier yetToVote(uint256 announcement) {
-        require(
-            !_announcements[announcement].positiveVoters[msg.sender] &&
-            !_announcements[announcement].negativeVoters[msg.sender]
-            ,
-            "You've already voted"
-        );
-        _;
+    function getTeam(uint256 id) external view returns (Team memory) {
+        return _teams[id];
     }
-
 
     // Staking
 	Staking _staking;
@@ -93,220 +74,40 @@ abstract contract Oracle {
 	function getTotalStakers() public view returns (uint256) {
 		return _staking.validators().length;
 	}
-    /*
-        Announcements can be:
-            New team has joined
-            A game is scheduled
-            A game starts
-            A team scores a goal
-                potential announcements:
-                    Player change
-                    Penalization
-                    Any string
-            A game ends
-    */
-    mapping(uint256 => Announcement) internal _announcements;
 
-    // struct Announcement {
-    //     uint256 announcementId;
-    //     mapping(address => bool) positiveVoters;
-    //     mapping(address => bool) negativeVoters;
-    //     uint32 positiveVotes;
-    //     uint32 negativeVotes;
-    //     AnnouncementState state;
-    // }
+    address[] internal _announcements;
 
     uint256 private _announcementsQuantity = 0; // no me gusta para nada esto ... constructor?
     event GeneralAnnouncement(address announcer, string announcement);
-    event TeamAnnouncement(address announcer, address teamId, string name);
-    event GameAnnouncement(address announcer, address gameId, uint256 date, address teamA, address teamB);
-    event GoalAnnouncement(address announcer, address gameId, uint256 minute, address awarder, uint8 jersey);
-    event StatusAnnouncement(address announcer, address gameId, uint8 status);
+    event TeamWasAnnounced(address announcer, address teamId, string name);
+    event GameWasAnnounced(address announcer, address gameId, uint256 date, address teamA, address teamB);
+    event GoalWasAnnounced(address announcer, address gameId, uint256 minute, address awarder, uint8 jersey);
+    event StatusWasAnnounced(address announcer, address gameId, uint8 status);
 
-    event AnnouncementSolidified(uint256 announcementId);
-    event AnnouncementDisproved(uint256 announcementId);
+    event AnnouncementSolidified(address announcement);
+    event AnnouncementDisproved(address announcement);
 
-    function announce(string calldata data) virtual external {}
 
-    function announceTeam(address teamId, string calldata name) virtual public returns (uint256 announcementId) {
-        _cTeams[teamId] = name;
+    function announce(address announcement) virtual public returns (uint256 announcementId) {
+        Announcement a = Announcement(announcement);
+        require(isStaking(a.announcer()), "You must stake to announce in this oracle");
+        require(a.getAnnouncementPosVotes() == 0 && a.getAnnouncementNegVotes() == 0, "This announcement has already been voted");
+        // perhaps check if it was already announced? queue logic?
+        uint256 index = _announcements.length;
+        _announcements.push(announcement);        
+        // we accept the announcement into out candidates
+        a.reduce(_games, _teams);
+        // emit TeamWasAnnounced(msg.sender, teamId, name);
 
-        emit TeamAnnouncement(msg.sender, teamId, name);
-        announcementId = createAnnouncement();
-
-        return announcementId;
+        return index;
     }
 
-    function announceGame(address gameId, uint256 date, address teamA, address teamB) virtual public returns (uint256 announcementId){
-        // needs to check if teamA and teamB has been announced.
+    // function getAnnouncedTeamName(address teamId) public view returns (string memory teamName) {
+    //     return _cTeams[teamId];
+    // }
 
-        Game memory game = Game({date: date, a: teamA, b: teamB,
-                                status: Constants.UNSTARTED,
-                                result: Constants.NOTRESULTYET,
-                                scoreA: Constants.NOTRESULTYET,
-                                scoreB: Constants.NOTRESULTYET});
-        _cGames[gameId] = game;
+    // function getAnnouncedGameDate(address gameId) public view returns (uint256 gameDate) {
+    //     return _cGames[gameId].date;
+    // }
 
-        announcementId = createAnnouncement();
-        emit GameAnnouncement(msg.sender, gameId, date, teamA, teamB);
-
-        return announcementId;
-    }
-
-    function announceGoal(address gameId, uint256 minute, address teamAwarder, uint8 jersey) virtual external returns (uint256 announcementId){
-        // Needs to check if game and team playing is valid
-
-        announcementId = createAnnouncement();
-
-        Goal memory goal = Goal({minute: minute, teamAwarder: teamAwarder, jersey: jersey});
-        _cGoals[announcementId] = goal;
-
-        emit GoalAnnouncement(msg.sender, gameId, minute, teamAwarder, jersey);
-
-        return announcementId;
-    }
-
-    function announceGameStatus(address gameId, uint8 status) virtual external returns (uint256 announcementId) {
-        announcementId = createAnnouncement();
-
-        GameStatus memory gameStatus = GameStatus({gameId: gameId, status: status});
-        _cGameStatus[announcementId] = gameStatus;
-
-        emit StatusAnnouncement(msg.sender, gameId, status);
-
-        return announcementId;
-    }
-
-    /*
-        Each type of announcement approval needs its approval function but only
-        one function (disproveAnnouncement) is necessary for disproval.
-    */
-    function approveTeamAnnouncement(uint256 announcementId, address teamId, string calldata teamName) virtual public yetToVote(announcementId) returns (bool wasSolidified) {
-        // Needs to check if this user has not approved this announcement before.
-        // Needs to check that input data matches _cTeams data.
-        // Needs to check if announcementId is a TeamAnnouncement.
-        // Needs to check if teamId is valid.
-        wasSolidified = approveAnnouncement(announcementId);
-
-        if (wasSolidified) {
-            _teams[teamId] = teamName;
-        }
-
-        return wasSolidified;
-    }
-
-    function approveGameAnnouncement(uint256 announcementId, address gameId) virtual public yetToVote(announcementId) returns (bool wasSolidified) {
-        // Needs to check if this user has not approved this announcement before.
-        // Needs to check if announcementId is a GameAnnouncement.
-        // Needs to check if gameId is valid.
-        wasSolidified = approveAnnouncement(announcementId);
-
-        if (wasSolidified) {
-            _games[gameId] = _cGames[gameId];
-        }
-
-        return wasSolidified;
-    }
-
-    function approveGoalAnnouncement(uint256 announcementId) virtual public yetToVote(announcementId) returns (bool wasSolidified) {
-        // Needs to check if this user has not approved this announcement before.
-        // Needs to check if announcementId is a GoalAnnouncement.
-        wasSolidified = approveAnnouncement(announcementId);
-
-        if (wasSolidified) {
-            _goals[announcementId] = _cGoals[announcementId];
-        }
-
-        return wasSolidified;
-    }
-
-    function approveGameStatusAnnouncement(uint256 announcementId, address gameId, uint8 newGameStatus) virtual public yetToVote(announcementId) returns (bool wasSolidified) {
-        // Needs to check if this user has not approved this announcement before.
-        // Needs to check if announcementId is a StatusAnnouncement.
-        // Needs to check if gameId is a valid game.
-        wasSolidified = approveAnnouncement(announcementId);
-
-        if (wasSolidified) {
-            _games[gameId].status = newGameStatus;
-
-            if (newGameStatus == 2) { // game has finished
-                uint8 scoreTeamA = _games[gameId].scoreA;
-                uint8 scoreTeamB = _games[gameId].scoreB;
-                if (scoreTeamA > scoreTeamB) {
-                    _games[gameId].result = 1; // A won
-                } else if (scoreTeamA == scoreTeamB) {
-                    _games[gameId].result = 3; // tie
-                } else if (scoreTeamA < scoreTeamB) {
-                    _games[gameId].result = 2; // B won
-                }
-            }
-        }
-
-        return wasSolidified;
-    }
-
-    function createAnnouncement() internal returns (uint256 announcementId) {
-        Announcement storage a = _announcements[_announcementsQuantity++];
-        a.announcementId = _announcementsQuantity;
-        a.positiveVotes = 0;
-        a.negativeVotes = 0;
-        a.state = AnnouncementState.NotEnoughVotes;
-
-        return a.announcementId;
-    }
-
-    function getAnnouncedTeamName(address teamId) public view returns (string memory teamName) {
-        return _cTeams[teamId];
-    }
-
-    function getAnnouncedGameDate(address gameId) public view returns (uint256 gameDate) {
-        return _cGames[gameId].date;
-    }
-
-    function approveAnnouncement(uint256 announcementId) internal yetToVote(announcementId) returns (bool wasSolidified) {
-        // Needs to check if this user has not approved this announcement before.
-        wasSolidified = false;
-        Announcement storage a = _announcements[announcementId];
-        a.positiveVotes += 1;
-        a.positiveVoters[msg.sender] = true;
-        uint256 totalStakers = getTotalStakers();
-
-        if (a.positiveVotes > totalStakers/2 && a.state == AnnouncementState.NotEnoughVotes) { // cuidado que es division entera
-            // solidify announcement
-            emit AnnouncementSolidified(announcementId); // , announcementType);
-            a.state = AnnouncementState.Solidified;
-            wasSolidified = true;
-        }
-
-        return wasSolidified;
-    }
-
-    function disproveAnnouncement(uint256 announcementId) public yetToVote(announcementId) returns (bool wasDisproved) {
-        Announcement storage a = _announcements[announcementId];
-        wasDisproved = false;
-        a.negativeVotes += 1;
-        a.negativeVoters[msg.sender] = true;
-        uint256 totalStakers = getTotalStakers();
-
-        if (a.negativeVotes > totalStakers/2 && a.state == AnnouncementState.NotEnoughVotes) { // cuidado que es division entera
-            // disprove announcement
-            emit AnnouncementDisproved(announcementId); // , announcementType);
-            a.state = AnnouncementState.Disproven;
-            wasDisproved = true;
-        }
-
-        return wasDisproved;
-    }
-
-    function getAnnouncementState(uint256 announcementId) public view returns (AnnouncementState state) {
-        return _announcements[announcementId].state;
-    }
-
-    function getAnnouncementPosVotes(uint256 announcementId) public view returns (uint32 posVotes) {
-        return _announcements[announcementId].positiveVotes;
-    }
-
-    function getAnnouncementNegVotes(uint256 announcementId) public view returns (uint32 negVotes) {
-        return _announcements[announcementId].negativeVotes;
-    }
 }
